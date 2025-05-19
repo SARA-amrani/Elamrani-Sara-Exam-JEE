1ff67b25b6087468be4b56b93c11762179dbdfeb
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-05-19 08:56:59 +0100

    Initial commit

---
+++ src/test/java/ma/elamrani/sara/ElamraniSaraExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elamrani.sara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElamraniSaraExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
